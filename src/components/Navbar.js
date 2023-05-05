c1c892837d8ad1475fdd075e911ae306208a5bf8
Joseph Musya <josephmusya254@gmail.com>
2023-05-05 11:33:27 +0300

    Modified the component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,69 @@
 import styles from './navbar.module.css';
+import profilePic from '../assets/profile_photo.png';
+import {FaPlus, FaMoneyBill, FaBell, FaTrophy, FaStar, } from 'react-icons/fa';
+import {MdSettings, MdBook, MdHelpCenter, MdArrowBack, MdGroup} from 'react-icons/md';
 export default function Navbar(){
     return (
         <nav>
             <div className={styles.logo}>
-                <strong>TMM Writers</strong>
+                <h1>Changia</h1>
             </div>
+            <div className={styles.profile}>
+                <div className={styles.photoBox}>
+                    <img src={profilePic} alt="Profile cover" />
+                </div>
+                <div className={styles.credentials}>
+                    <p>John Doe</p>
+                    <p>Donations Made: 53</p>
+                </div>
+            </div>
+                        
             <div className={styles.navElements}>
-                <li>Add Order</li>                
-                <li>Add Writer</li>
-                <li>Notification</li>
-                <li>Profile</li>
+                <div>
+                    <FaPlus />
+                    <li>Start Campaign</li>
+                </div>                
+                <div >
+                    <FaMoneyBill />
+                    <li>Recharge</li>
+                </div>
+                <div>
+                    <FaBell />
+                    <li>Notification</li>
+                </div>                
+                {/* <div>
+                    <FaTrophy />
+                    <li>Badges</li>
+                </div> */}
+                {/* <div>
+                    <li>History</li>
+                </div> */}
+                <div>
+                    <MdGroup />
+                    <li>Backer community</li>   
+                </div>
+                <div className='divider'></div>             
+                <div>
+                    <FaStar />
+                    <li>Review & Rating</li>
+                </div>  
+                <div>
+                    <MdSettings />
+                    <li>Privacy & settings</li>  
+                </div>
+                <div>
+                    <MdBook />
+                    <li>Terms & Policies</li> 
+                </div>        
+                <div>
+                    <MdHelpCenter />
+                    <li>Support</li>
+                </div>
+                <div className="divider"></div>
+                <div>
+                    <MdArrowBack />
+                    <li>Logout</li>
+                </div>
             </div>
         </nav>
     )
