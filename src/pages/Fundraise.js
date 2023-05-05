60f3184e1853082714255d64b79fa77081fae476
Joseph Musya <josephmusya254@gmail.com>
2023-05-05 15:27:55 +0300

    Modified

--- src/pages/Fundraise.js
+++ src/pages/Fundraise.js
@@ -1,6 +1,14 @@
 import styles from './fundraise.module.css';
 import placeholderImg from '../assets/placeholder.png'
+import { useState } from 'react';
+import DonationModal from '../components/modals/DonationModal';
+
 export default function Fundraise(props){
+    let [donateModal, setDonateModal] = useState(false);
+
+    const toggleDonationModal = () => {
+        setDonateModal = setDonateModal(!donateModal);
+    }
     return (
         <div className={styles.fundraise}>
             <h2>{props.title}</h2>
@@ -13,8 +21,16 @@ export default function Fundraise(props){
                 <p>Progress</p>
             </div>
             <div className={styles.donate}>
-                <button>D o n a t e</button>
-            </div>
+                <button onClick={toggleDonationModal}>D o n a t e</button>
+            </div>            
+            {
+                donateModal?(  
+                    <div>
+                        <div className={styles.overlay} onClick={toggleDonationModal}></div>
+                        <DonationModal title={props.title} />
+                    </div>                  
+                ):<div></div>
+            }
         </div>            
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,18 @@
 import styles from './homepage.module.css';
 import Fundraise from './Fundraise';
-export default function Home(){
+export default function Home(){        
     return (
-            <div className={styles.home}>
-                <div className={styles.topic}>
-                    <h1>Fundraising Campaigns - Quick & Transparent</h1>
-                </div>
-                <div className={styles.fundraise}>
-                    <Fundraise title='Childrens home fundraise' />
-                    <Fundraise title='Wedding fundraise' />
-                    <Fundraise title='Fees drive for John'/>
-                    <Fundraise title='ACK St Peters church construction' />
-                    <Fundraise title='Std 6 class construction'/>
-                </div>
+        <div className={styles.home}>
+            <div className={styles.topic}>
+                <h1>Fundraising Campaigns - Quick & Transparent</h1>
             </div>
+            <div className={styles.fundraise}>
+                <Fundraise title='Childrens home fundraise' />
+                <Fundraise title='Wedding fundraise' />
+                <Fundraise title='Fees drive for John'/>
+                <Fundraise title='ACK St Peters church construction' />
+                <Fundraise title='Std 6 class construction'/>
+            </div>
+        </div>
     )
 }
